23f254d8343a39e962d259823d7af739ffd2dcb2
harish <harish.ravishankar16@gmail.com>
2018-01-23 15:09:51 -0500

    reminder app built using redux

---
+++ reminderpro/src/actions/index.js
@@ -0,0 +1,19 @@
+import {ADD_REMINDER,DELETE_REMINDER} from '../constants';
+
+export const addReminder=(text,dueDate) =>{
+  const action ={
+    type:ADD_REMINDER,
+    text,
+    dueDate
+  }
+  console.log(action);
+  return action;
+}
+export const deleteReminder=(id) => {
+  const action={
+    type:DELETE_REMINDER,
+    id
+  }
+console.log('deleting actions',action);
+return action;
+}
